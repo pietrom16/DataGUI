875d83fcf48fc4da4256dd34f333462cb1cfdcb9
PietroM16 <pietrom16@gmail.com>
2017-05-28 17:26:57 +0100

    - Implemented in TreeModel: data(), headerData(), index(), parent(), flags(), getItem().

--- TreeModel.cpp
+++ TreeModel.cpp
@@ -38,6 +38,73 @@ TreeModel::~TreeModel()
 }
 
 
+QVariant TreeModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    TreeItem *item = static_cast<TreeItem*>(index.internalPointer());
+
+    if (role == Qt::DisplayRole)
+    {
+        if (index.column() == 0)
+            return item->Tag();
+
+        if (index.column() == 1)
+            return item->Val();
+    }
+
+    return QVariant();
+}
+
+
+QVariant TreeModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole)
+        return QVariant();
+
+    if (orientation == Qt::Horizontal)
+        return headers.value(section);
+    else
+        return QVariant();
+}
+
+
+QModelIndex TreeModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if (!hasIndex(row, column, parent))
+        return QModelIndex();
+
+    TreeItem *parentItem;
+
+    if (!parent.isValid())
+        parentItem = rootItem;
+    else
+        parentItem = static_cast<TreeItem*>(parent.internalPointer());
+
+    TreeItem *childItem = parentItem->Child(row);
+    if (childItem)
+        return createIndex(row, column, childItem);
+    else
+        return QModelIndex();
+}
+
+
+QModelIndex TreeModel::parent(const QModelIndex &index) const
+{
+    if (!index.isValid())
+        return QModelIndex();
+
+    TreeItem *childItem = static_cast<TreeItem*>(index.internalPointer());
+    TreeItem *parentItem = childItem->Parent();
+
+    if (parentItem == rootItem)
+        return QModelIndex();
+
+    return createIndex(parentItem->row(), 0, parentItem);
+}
+
+
 int TreeModel::rowCount(const QModelIndex &parent) const
 {
     TreeItem *parentItem = getItem(parent);
@@ -49,6 +116,15 @@ int TreeModel::rowCount(const QModelIndex &parent) const
 int TreeModel::columnCount(const QModelIndex & /* parent */) const
 {
     return rootItem->ColumnCount();
+}
+
+
+Qt::ItemFlags TreeModel::flags(const QModelIndex &index) const
+{
+    if (!index.isValid())
+        return 0;
+
+    return Qt::ItemIsEditable | QAbstractItemModel::flags(index);
 }
 
 
@@ -104,6 +180,17 @@ bool TreeModel::LoadJson(const QByteArray &json)
     return false;
 }
 
+
+TreeItem *TreeModel::getItem(const QModelIndex &index) const
+{
+    if (index.isValid()) {
+        TreeItem *item = static_cast<TreeItem*>(index.internalPointer());
+        if (item)
+            return item;
+    }
+
+    return rootItem;
+}
 
 
 } // GuiUtilities
